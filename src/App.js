ec48d7125b9a974da305f12749b3a11e7ba33d6d
Jiye Han <mwldpm@naver.com>
2020-05-27 18:48:00 +0900

    react-class,state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
 import React from 'react';
 
-const foodILike = [
-  {
-    id:1,
-    name : "kimchi",
-    image: "https://cdn.pixabay.com/photo/2019/03/09/15/12/kimchi-4044268_1280.jpg"
-  },
-  {
-    id:2,
-    name : "ramen",
-    image: "https://cdn.pixabay.com/photo/2020/03/31/10/37/ramen-4987117_1280.jpg"
-  },
-  {
-    id:3,
-    name : "snack",
-    image: "https://cdn.pixabay.com/photo/2016/09/18/20/25/candy-1678933_1280.jpg"
-  } 
-]
+//react는 자동적으로 class component의 render method를 실행한다!
+class App extends React.Component{
 
+  state = {
+    //변화하는 데이터를 담기 위해 state가 필요하다
+    count: 0,
+    isLoading: true
+  };
+  add = ( ) => {
+    //console.log("add");
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = ( ) => {
+    //console.log("miinus");
+    this.setState(current => ({count: current.count - 1}));
+  };
 
-function Food({name,picture}) {
-  return(
-     <div>
-    <h1>I like {name} </h1>
-    <img src = {picture} alt = {name}/>
-  </div>
-  );
-}
+  componentDidMount(){
+    //mount 된것을 알려줌
+    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 3000);
+  }
+  componentDidUpdate(){
+    //update 된것을 알려줌
+    console.log("i just update");
+  }
+  componentWillUnmount(){
+    //unmount 된것을 알려줌
+    console.log("GoodBye, unmount");
+  }
 
-function App() {
-  return <div>
-            <h3>Hello!!!!!!!!</h3>
-            {foodILike.map(dish =>(
-               <Food key={dish.id} name={dish.name} picture={dish.image} />
-               ))}
-                        
-        </div>;
+    //매 순간 setState를 호출할 때마다 react는 새로운 state와 함께 render function을 호출한다.
+  render() {
+    console.log("i'm rendering");
+    const {isLoading} =  this.state;
+    return (
+    <div>
+
+      {isLoading ? "Loading...":"We are ready!"}
+      <h1> The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    );
+  }
 }
 
 export default App;
